24d02ec6d71ba1bd70d73c8a29e151b02211b604
Wiston Venera Macías <wisvem@hotmail.com>
2020-12-29 17:19:50 -0500

    - Added chrome web driver for windows and linux. - Added selenium for render dynamic pages. - Added render_page() function for render full pages

--- web_scraping.py
+++ web_scraping.py
@@ -1,25 +1,38 @@
 #!/usr/bin/python
+import time
 import requests
-from bs4 import BeautifulSoup as soup
+from bs4 import BeautifulSoup as bs
+from selenium import webdriver
 
-for page in range(1, 6):
-    # Collect and parse for every page.
-    url = 'https://www.falabella.com.co/falabella-co/category/cat1360967/Televisores?page=' + str(page)
-    html = requests.get(url)
-    bs = soup(html.text, 'html.parser')
+coptions = webdriver.ChromeOptions()
+#coptions.add_argument('--ignore-certificate-errors')
+coptions.add_argument('--incognito')
+#coptions.add_argument('--headless')
 
-    # Pull all div tag with a specific tag. A list of products container.
-    containers = bs.find_all(class_="jsx-1585533350")
 
-    # Pull and print for every product the brand, name and price.
-    print()
-    print("############### Page - {} ###############".format(page))
-    print()
+def render_page(url):
+    driver = webdriver.Chrome(chrome_options=coptions)
+    driver.get(url)
+    time.sleep(3)
+    r = driver.page_source
+    # driver.quit()
+    return r
+
+
+for page in range(1,6):
+    url = 'https://www.falabella.com.co/falabella-co/category/cat1360967/Televisores?page=' + \
+        str(page)
+    render = render_page(url)
+    soup = bs(render, "html.parser")
+    containers = soup.find_all(class_="jsx-1585533350")
+    # print(soup.prettify())
     for container in containers:
         brand = container.find('b')
         name = container.find('span').b
         price = container.find('div', class_="jsx-3342506598").span
+        print("--------------------------------")
         print("Marca: {}".format(brand.text.strip()))
         print("Nombre: {}".format(name.text.strip()))
         print("Precio: {}".format(price.text.strip()))
-        print("--------------------------------")
+    print(len(containers))
+
